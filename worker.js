52ed468f7e8f588dc4c62a18ccd23bf3cdb82f4e
Nathan Clevenger <nateclev@gmail.com>
2022-07-29 00:51:26 -0500

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,11 @@
+export default {
+  fetch: (req, env, ctx) => {
+    // rewrite to Github pages
+    // Add tailwind css or twind reference
+    // Add favicon
+    // Add Schema.org JSONLD
+    // Add Header
+    // Add Footer
+//     return res
+  }
+}
